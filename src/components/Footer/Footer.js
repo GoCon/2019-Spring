b8ba4bfac108e641fd3b00a4a385f384a824d87e
aimof <aimof.aimof@gmail.com>
2019-04-08 08:12:57 +0900

    Add links with Sponsors logos.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,8 @@ import styles from './Footer.module.scss';
 const Footer = () => (
     <footer class={styles.footer}>
         <h2>Sponsors</h2>
-        <a href="#"><img src="/media/sponsors/01_freee.png"></img></a>
-        <a href="#"><img src="/media/sponsors/02_cloudsign.png"></img></a>
-        <a href="#"><img src="/media/photo.jpg"></img></a>
-        <a href="#"><img src="/media/photo.jpg"></img></a>
-        <a href="#"><img src="/media/photo.jpg"></img></a>
-        <a href="#"><img src="/media/photo.jpg"></img></a>
-        <a href="#"><img src="/media/photo.jpg"></img></a>
-        <a href="#"><img src="/media/photo.jpg"></img></a>
+        <a href="https://www.freee.co.jp/"><img src="/media/sponsors/01_freee.png"></img></a>
+        <a href="https://www.cloudsign.jp/"><img src="/media/sponsors/02_cloudsign.png"></img></a>
     </footer>
 )
 
