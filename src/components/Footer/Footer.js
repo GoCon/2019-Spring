75c6048fcc0206fbd65eb0d68fedc3c982934012
mi-bear <kumajet@gmail.com>
2019-04-13 15:41:50 +0900

    Add sponsor logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,7 @@ const Footer = () => (
         <h2>Sponsors</h2>
         <a href="https://www.freee.co.jp/"><img src="/media/sponsors/01_freee.png"></img></a>
         <a href="https://www.cloudsign.jp/"><img src="/media/sponsors/02_cloudsign.png"></img></a>
+        <a href="https://wantedlyinc.com/ja/presentations"><img src="/media/sponsors/03_wantedly.png"></img></a>
     </footer>
 )
 
