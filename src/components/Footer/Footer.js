bcb4fba95d7de340f662fa0f35acc4897e8290e3
aimof <aimof.aimof@gmail.com>
2019-04-17 21:56:09 +0900

    Centoring Sponsors logo and add one.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,12 @@ import styles from './Footer.module.scss';
 const Footer = () => (
     <footer class={styles.footer}>
         <h2>Sponsors</h2>
-        <a href="https://www.freee.co.jp/"><img src="/media/sponsors/01_freee.png"></img></a>
-        <a href="https://www.cloudsign.jp/"><img src="/media/sponsors/02_cloudsign.png"></img></a>
-        <a href="https://wantedlyinc.com/ja/presentations"><img src="/media/sponsors/03_wantedly.png"></img></a>
+        <div class={styles.row}>
+            <div><a href="https://www.freee.co.jp/"><img src="/media/sponsors/01_freee.png"></img></a></div>
+            <div><a href="https://www.cloudsign.jp/"><img src="/media/sponsors/02_cloudsign.png"></img></a></div>
+            <div><a href="https://wantedlyinc.com/ja/presentations"><img src="/media/sponsors/03_wantedly.png"></img></a></div>
+            <div><a href="https://about.mercari.com/"><img src="/media/sponsors/04_mercari_logo_corporate.png"></img></a></div>
+        </div>
     </footer>
 )
 
