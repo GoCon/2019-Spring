fbfb39345b0e86cc56cc0ba0ab8cdf8d45fd904a
mi-bear <kumajet@gmail.com>
2019-04-25 23:41:14 +0900

    Add Sponsor Logo mediado

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,9 +8,10 @@ const Footer = () => (
             <div><a href="https://www.cloudsign.jp/"><img src="/media/sponsors/01_cloudsign.png"></img></a></div>
             <div><a href="https://dena.com"><img src="/media/sponsors/02_dena.png"></img></a></div>
             <div><a href="https://www.freee.co.jp/"><img src="/media/sponsors/03_freee.png"></img></a></div>
-            <div><a href="https://about.mercari.com/"><img src="/media/sponsors/04_mercari_logo_corporate.png"></img></a></div>
-            <div><a href="https://www.recruit.co.jp/"><img src="/media/sponsors/05_recruit.png"></img></a></div>
-            <div><a href="https://wantedlyinc.com/ja/presentations"><img src="/media/sponsors/06_wantedly.png"></img></a></div>
+            <div><a href="https://www.mediado.jp/"><img src="/media/sponsors/04_mediado.png"></img></a></div>
+            <div><a href="https://about.mercari.com/"><img src="/media/sponsors/05_mercari_logo_corporate.png"></img></a></div>
+            <div><a href="https://www.recruit.co.jp/"><img src="/media/sponsors/06_recruit.png"></img></a></div>
+            <div><a href="https://wantedlyinc.com/ja/presentations"><img src="/media/sponsors/07_wantedly.png"></img></a></div>
         </div>
     </footer>
 )
